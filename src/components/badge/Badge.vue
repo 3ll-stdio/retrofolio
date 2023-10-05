c90425335cde729592ca2648462bc23db4faa5f0
Fotios Pechlivanis <fotispechli@gmail.com>
2023-10-05 16:10:43 +0300

    Add animation for badge

--- src/components/badge/Badge.vue
+++ src/components/badge/Badge.vue
@@ -23,6 +23,10 @@ import { Logo } from "@components";
 .outer {
   box-shadow: 0px -2px 2px 0px rgba(255, 255, 255, 0.4) inset,
     0px 2px 2px 0px rgba(0, 0, 0, 0.4) inset;
+
+  &:hover .logo {
+    animation: flip 2s linear infinite;
+  }
 }
 
 .inner {
@@ -34,5 +38,25 @@ import { Logo } from "@components";
   width: var(--spacing-2xl);
   height: var(--spacing-2xl);
   aspect-ratio: 1 / 1;
+  perspective: 400px;
+  animation: flip 2s linear;
+}
+
+@keyframes flip {
+  0% {
+    transform: rotateY(0deg) translateY(0rem);
+  }
+  25% {
+    transform: rotateY(90deg) translateY(-0.1rem);
+  }
+  50% {
+    transform: rotateY(180deg) translateY(0rem);
+  }
+  75% {
+    transform: rotateY(90deg) translateY(-0.1rem);
+  }
+  100% {
+    transform: rotateY(0deg) translateY(0rem);
+  }
 }
 </style>
