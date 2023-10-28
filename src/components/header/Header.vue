fdbd07d7872342614725b224120d0c924f290d63
Fotios Pechlivanis <fotispechli@gmail.com>
2023-10-29 02:59:40 +0300

    Make header title reactive

--- src/components/header/Header.vue
+++ src/components/header/Header.vue
@@ -1,7 +1,37 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { computed } from "vue";
+import { useRoute } from "vue-router";
+
+const route = useRoute();
+
+console.log(route, route.name)
+
+const pageTitle = computed(() => {
+  switch (route.path) {
+    case "/home":
+      return "Retrofolio's Official Site";
+    case "/about":
+      return "Retrofolio's Official CV";
+    case "/projects":
+      return "Retrofolio's Official Projects";
+    case "/projects/project-1":
+      return "Project 1";
+    case "/projects/project-2":
+      return "Project 2";
+    case "/projects/project-3":
+      return "Project 3";
+    case "/projects/project-4":
+      return "Project 4";
+    default:
+      return "Retrofolio's Official Site";
+  }
+});
+</script>
 
 <template>
-  <header><h1 class="p p2-semibold">Official Site</h1></header>
+  <header>
+    <h1 class="p p2-semibold">{{ pageTitle }}</h1>
+  </header>
 </template>
 
 <style scoped>
